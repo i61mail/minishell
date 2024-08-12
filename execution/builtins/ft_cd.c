048ee3b7d62f9cec45b867bfbca804dd197eeac4
1nn3rV01d <151653610+1nn3rV01d@users.noreply.github.com>
2024-08-12 19:10:04 +0100

    ignored parsing

--- execution/builtins/ft_cd.c
+++ execution/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: mait-lah <mait-lah@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 17:34:46 by mait-lah          #+#    #+#             */
-/*   Updated: 2024/08/11 18:49:48 by mait-lah         ###   ########.fr       */
+/*   Updated: 2024/08/12 19:06:19 by mait-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int	ft_cd(t_vars *vars,t_list *comm,t_env *envir)
 	(void)vars;
 	(void)comm;
 	(void)envir;
-    //char *temp;
 
-	//temp = getcwd(NULL, 0);
-	//printf("%s\n",temp);
+
 	if (comm && comm->next)
 	{
 		comm = comm->next;
-		chdir(comm->content);
-		printf("%s\n",comm->content);
-	}	
+		if (chdir(comm->content))
+			printf("cd:%s: No such file or directory\n",comm->content);
+		
+		//printf("%s\n",comm->content);
+	}
 	//temp = getcwd(NULL, 0);
 	//printf("%s\n",temp);
 	return (0);
