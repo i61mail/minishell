e124d0ad3c6c5b06442e470ac9d85afacb8326af
Ismail Rkik <isrkik@e3r3p11.1337.ma>
2024-09-05 16:21:30 +0100

    remove what i did before hh

--- execution/builtins/ft_env.c
+++ execution/builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: isrkik <isrkik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 03:36:11 by mait-lah          #+#    #+#             */
-/*   Updated: 2024/09/05 16:14:28 by isrkik           ###   ########.fr       */
+/*   Updated: 2024/09/05 16:19:49 by isrkik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_env(t_env *envir, t_vars *vars)
 	temp = envir;
 	while (temp)
 	{
-		if (temp->value && ft_strncmp(temp->key, "_PWD\0", 5) != 0 && ft_strncmp(temp->key, "PATH\0", 5) != 0)
+		if (temp->value && ft_strncmp(temp->key, "_PWD\0", 5) != 0)
 		{
 			ft_putstr_fd(temp->key, vars->pfd[1]);
 			ft_putchar_fd('=', vars->pfd[1]);
