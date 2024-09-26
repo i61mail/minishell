7893a2b75b6dd935dbe7669dcd548f6d05f3cda5
Ismail Rkik <isrkik@e1r5p11.1337.ma>
2024-09-26 09:56:41 +0100

    fix exit status for cat << a

--- execution/ft_execute.c
+++ execution/ft_execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_execute.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: i61mail <i61mail@student.42.fr>            +#+  +:+       +#+        */
+/*   By: isrkik <isrkik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 14:15:40 by mait-lah          #+#    #+#             */
-/*   Updated: 2024/09/25 20:18:29 by i61mail          ###   ########.fr       */
+/*   Updated: 2024/09/26 09:54:56 by isrkik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -178,6 +178,11 @@ void	ft_run(t_vars *vars, t_list *comm, t_env **envir)
 	}
 	if (waitpid(id, &pid, 0) > 0)
 	{
+		if (vars->exit_status == 130 && vars->is_signal == 1)
+		{
+			vars->exit_status = 1;
+			return ;
+		}
     	if (WIFEXITED(pid))
         	vars->exit_status = WEXITSTATUS(pid);
 		else if (WIFSIGNALED(pid))
