ca31717cf786acdd157b9cc9d804e741f3b929f5
Ismail Rkik <isrkik@e1r4p11.1337.ma>
2024-10-01 15:33:50 +0100

    fix norm shell_level

--- utils/shell_level.c
+++ utils/shell_level.c
@@ -6,7 +6,7 @@
 /*   By: isrkik <isrkik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 15:09:22 by isrkik            #+#    #+#             */
-/*   Updated: 2024/10/01 15:11:01 by isrkik           ###   ########.fr       */
+/*   Updated: 2024/10/01 15:33:10 by isrkik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,30 @@ static void	init_shell(long long *increm, int *is, long long *old_increm)
 	*old_increm = 0;
 }
 
+void	shell_level2(t_env **env, int *is, long long *old_increm,
+			long long *increm)
+{
+	while (env && *env)
+	{
+		if (ft_strncmp((*env)->key, "SHLVL\0", 6) == 0)
+		{
+			shell_level_utils(is, old_increm, increm, *env);
+			if (*increm == 1000)
+			{
+				free((*env)->value);
+				(*env)->value = "";
+				*increm = 0;
+				break ;
+			}
+			else if (*increm > 1000)
+				warning_error(increm);
+			free((*env)->value);
+			(*env)->value = ft_itoa(*increm);
+		}
+		(*env) = (*env)->next;
+	}
+}
+
 int	shell_level(t_env **envir)
 {
 	t_env		*env;
@@ -46,25 +70,7 @@ int	shell_level(t_env **envir)
 
 	init_shell(&increm, &is, &old_increm);
 	env = *envir;
-	while (env)
-	{
-		if (ft_strncmp(env->key, "SHLVL\0", 6) == 0)
-		{
-			shell_level_utils(&is, &old_increm, &increm, env);
-			if (increm == 1000)
-			{
-				free(env->value);
-				env->value = "";
-				increm = 0;
-				return (0);
-			}
-			else if (increm > 1000)
-				warning_error(&increm);
-			free(env->value);
-			env->value = ft_itoa(increm);
-		}
-		env = env->next;
-	}
+	shell_level2(&env, &is, &old_increm, &increm);
 	if (is == 0)
 		add_to_node(ft_strdup("SHLVL"), ft_strdup("1"), envir);
 	return (0);
