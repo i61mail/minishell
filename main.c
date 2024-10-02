6768bcd8863f02c4f56f9f224a9cdf0339688fb2
Ismail Rkik <isrkik@e1r4p11.1337.ma>
2024-10-02 20:02:29 +0100

    i have no idea if its the correct way for that function

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isrkik <isrkik@student.42.fr>              +#+  +:+       +#+        */
+/*   By: i61mail <i61mail@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 17:09:34 by isrkik            #+#    #+#             */
-/*   Updated: 2024/09/04 13:21:16 by isrkik           ###   ########.fr       */
+/*   Updated: 2024/10/01 22:15:35 by i61mail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	ft_pars_comm(t_vars *vars, t_list **comm, t_env **envir)
 {
 	int		i;
+
 	i = 0;
 	while (vars->read[i] != '\0')
 	{
@@ -26,120 +27,70 @@ int	ft_pars_comm(t_vars *vars, t_list **comm, t_env **envir)
 		else if (ft_issep(vars->read[i]))
 		{
 			if (ft_aresep(vars, &i, comm, envir) == -1)
-				return (-1);
+				return (is_sep(vars));
+			if (vars->exit_status == 130 && vars->is_signal == 1)
+				break ;
 		}
 		if (quotes(vars, &i, comm, envir) == -1)
 			return (-1);
 	}
-	// t_list	*temp;
-	// temp = *comm;
-	// while (temp)
-	// {
-	// 	printf("%s   && %d\n", temp->content, temp->type);
-	// 	temp = temp->next;
-	// }
 	return (0);
 }
 
-int	three_vars(t_env **envir)
+int	pars_exec(t_vars *vars, t_list *comm, t_env **envir)
 {
-	add_to_node(ft_strdup("PWD"), ft_strdup("/Users/isrkik/Desktop/minishell"), envir);
-	add_to_node(ft_strdup("PATH"), ft_strdup("/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Library/Apple/usr/bin"), envir);
-	add_to_node(ft_strdup("SHLVL"), ft_strdup("1"), envir);
-	add_to_node(ft_strdup("_"), ft_strdup("/usr/bin/env"), envir);
-	add_to_node(ft_strdup("OLDPWD"), NULL, envir);
-	return (0);
-}
-
-int	shell_level(t_env **envir)
-{
-	t_env 		*env;
-	long long	increm;
-	long long 	old_increm;
-	int			is;
-
-	increm = 0;
-	is = 0;
-	old_increm = 0;
-	env = *envir;
-	while (env)
+	add_history(vars->read);
+	if (ft_pars_comm(vars, &comm, envir) != -1)
 	{
-		if (ft_strcmp(env->key, "SHLVL") == 0)
-		{
-			is = 1;
-			old_increm = ft_atoi(env->value);
-			increm = old_increm + 1;
-			if (increm > 2147483647 || increm < 0)
-				increm = 0;
-			else if (increm == 1000)
-			{
-				env->value = "";
-				increm = 0;
-				break ;
-			}
-			else if (increm > 1000)
-			{
-				printf("warning: shell level (%lld) too high, resetting to 1\n", increm);
-				increm = 1;
-			}
-			free(env->value);
-			env->value = ft_itoa(increm);
-			
-		}
-		env = env->next;
-	}
-	if (is == 0)
-		add_to_node(ft_strdup("SHLVL"), ft_strdup("1"), envir);
-	return (0);
-}
-
-void	init_vars(t_list **comm, t_vars *vars, t_env **envir, char **env)
-{
-	*envir = NULL;
-	*comm = NULL;
-	vars->read = NULL;
-	vars->token = NULL;
-	vars->catsh = 0;
-	vars->befor_sing = 0;
-	vars->bef_spac = 0;
-	vars->len = 0;
-	vars->start = 0;
-	vars->exit_status = 0;
-	vars->old_fd = 0;
-	vars->pipe = 0;
-	vars->numofpipes = 0;
-	vars->heredoc_fd = 0;
-	vars->flag_splite = 0;
-	vars->quoted = 0;
-	vars->del_type = 0;
-	strcpy_env(envir, env);
-	if (!*envir)
-		three_vars(envir);
-	shell_level(envir);
-}
-
-int	pars_exec(t_vars vars, t_list *comm, t_env **envir)
-{
-	add_history(vars.read);
-	if (ft_pars_comm(&vars, &comm, envir) != -1)
-	{
-		// char *str = get_next_line(vars.heredoc_fd);
-		// while (str)
+		// t_list *temp = comm;
+		// while (temp)
 		// {
-		// 	printf("gnl == %s", str);
-		// 	str = get_next_line(vars.heredoc_fd);
+		// 	printf("c:%s,t:%d\n", temp->content, temp->type);
+		// 	temp = temp->next;
 		// }
-		if (comm && comm->content[0] != '\0')
-		{
-			ft_execute(&vars, comm, envir);
-		}
-		free(vars.read);
+		ft_execute(vars, comm, envir);
+		free(vars->read);
 	}
 	else
-	{
-		free(vars.read);
-	}
+		free(vars->read);
 	return (0);
+}
+
+void	ft_readline_test(t_vars *vars, t_list *comm, t_env **envir, int ac, char **av)
+{
+
+	if (ac == 3)
+	{
+		vars->read = ft_strdup(av[2]);
+		if (!vars->read)
+		{
+			return ;
+		}
+		pars_exec(vars, comm, envir);
+	}
+}
+
+void	ft_readline(t_vars *vars, t_list *comm, t_env **envir, int ac)
+{
+	while (1)
+	{
+		tcsetattr(0, 0, &vars->reset);
+		if (ac == 1)
+		{
+			vars->read = readline("minishell> ");
+			if (!vars->read)
+			{
+				ft_putstr_fd("exit\n", 1);
+				break ;
+			}
+			pars_exec(vars, comm, envir);
+		}
+		else
+		{
+			ft_env_free(envir);
+			break ;
+		}
+	}
 }
 
 int	main(int ac, char **av, char **env)
@@ -149,24 +100,13 @@ int	main(int ac, char **av, char **env)
 	t_env	*envir;
 
 	(void)av;
-	init_vars(&comm, &vars, &envir, env);
-	while (1)
-	{
-		if (ac == 1)
-		{
-			vars.read = readline("minishell> ");
-			if (!vars.read)
-			{
-				free_all(vars.read, &comm, &envir);
-				break ;
-			}
-			pars_exec(vars, comm, &envir);
-		}
-		else
-		{
-			ft_env_free(&envir);
-			break ;
-		}
-	}
-	return (0);
+	main_init_vars(&comm, &vars, &envir, env);
+	tcgetattr(0, &vars.reset);
+	signal(SIGINT, handle_ctrlc);
+	signal(SIGQUIT, SIG_IGN);
+	rl_catch_signals = 0;
+	// ft_readline(&vars, comm, &envir, ac);
+	ft_readline_test(&vars, comm, &envir, ac, av);
+	exit(vars.exit_status);
 }
+//exit 3 cv " "
